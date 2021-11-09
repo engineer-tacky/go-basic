17a24a194b482f9abbcbf347ff0fd6d6561cfc43
Tsubasa Takimoto <tsubasa801@gmail.com>
2021-11-10 07:23:50 +0900

    constによる定数宣言

--- hello.go
+++ hello.go
@@ -2,26 +2,16 @@ package main
 
 import "fmt"
 
-// 変数の宣言 (varをつけると関数外でも宣言可能)
-var (
-	i    int     = 1
-	f64  float64 = 1.2
-	s    string  = "test"
-	t, f bool    = true, false
+const Name = "nekopunch"
+
+const (
+	Country = "Japan"
+	Birth   = 2021
 )
 
 func main() {
-	fmt.Println(i, f64, s, t, f)
-
-	// 変数宣言省略形 (関数内でしか記述できない)
-	xi := 1
-	// 型を明示的に指定する場合は、varを使用する
-	var xf32 float32 = 1.2
-	xs := "test"
-	xt, xf := true, false
-	fmt.Println(xi, xf32, xs, xt, xf)
-
-	// xf32の型を調べる
-	fmt.Printf("%T\n", xf32)
-
+	// 再代入は、エラーになります。
+	// cannot assign to Name (declared const)
+	// Name = "aaaaa"
+	fmt.Println(Name, Country, Birth)
 }
