cf30babc617d2b30312864764f41cde17ebf654b
Tsubasa Takimoto <tsubasa801@gmail.com>
2021-11-11 06:10:48 +0900

    スライスでの配列宣言追加

--- array.go
+++ array.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
-func Fruit() {
+func printArray() {
 	// 固定長の配列(2つまで)
-	var fruit [2]string
-	fruit[0] = "banana"
-	fruit[1] = "apple"
+	var fruits [2]string
+	fruits[0] = "banana"
+	fruits[1] = "apple"
 
 	// 3つ目を追加すると、エラーとなる
-	// first argument to append must be slice; have [2]string
-	// fruit = append(fruit, "orange")
+	// firsts argument to append must be slice; have [2]string
+	// fruits = append(fruits, "orange")
 
-	fmt.Println(fruit)
+	fmt.Println(fruits)
+
+	// 可変長の配列
+	var money []int = []int{1000, 2000}
+	money = append(money, 3000)
+
+	fmt.Println(money)
 }
