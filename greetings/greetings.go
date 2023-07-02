58d6b16105867cda62eb4f3e5fe2cc9b01587aad
engineer_tacky <tsubasa801@gmail.com>
2023-07-02 16:12:26 +0900

    テストを追加

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -14,7 +14,8 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprintf(randomFormat())
 	return message, nil
 }
 
